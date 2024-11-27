f3cfefd7cca9a519a3ea6d1638438dd972c50e0a
Burak Akdemir <brakdemir@gmail.com>
2024-11-28 00:07:01 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const sessionConfig = session({
     cookie: {
         maxAge: parseInt(process.env.CSRFT_EXPIRESIN), // Used for expiration time.
         sameSite: 'strict', // Cookies will only be sent in a first-party context. 'lax' is default value for third-parties.
-        httpOnly: true, //Ensures the cookie is sent only over HTTP(S)
+        httpOnly: true, //Mitigate the risk of a client side script accessing the cookie.
         domain: process.env.DOMAIN, //Used to compare against the domain of the server in which the URL is being requested.
         secure: false // Ensures the browser only sends the cookie over HTTPS. false for localhost.
     }
